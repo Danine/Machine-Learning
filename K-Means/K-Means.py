3fe70ed42bf8434b4bb6655a339e3edf61f46763
Tyson <381980920@qq.com>
2018-12-13 01:55:27 +0800

    日常进度

---
+++ K-Means/K-Means.py
@@ -0,0 +1,16 @@
+import matplotlib.image as img
+import matplotlib.pyplot as plt
+import numpy as np
+
+value = img.imread('D:\\GitHub\\Machine-Learning\\K-Means\\bird_small.tiff')
+row = np.size(value,0)
+col = np.size(value,1)
+print(value.shape)
+#u为随机采样16种颜色作为聚类
+u = np.zeros((16,3))
+for i in range(16):
+	for j in range(3):
+		u[i,j] = np.random.randint(255)
+print(u)
+# plt.imshow(value)
+# plt.show()
